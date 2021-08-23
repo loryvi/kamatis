be9cdacb7e7d1cdda2353c557529d22779682846
lory!! <61609681+loryvi@users.noreply.github.com>
2021-08-23 18:30:36 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+def appstart():
+    pomodoro = int(input("Enter 1 to start pomodoro: "))
+    t = 10
+    if(pomodoro == 1):
+        while t:
+            mins = t // 60
+            secs = t % 60 
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")
+            time.sleep(1) #delays the overwrite by one second
+            t -= 1 #overwrite the timer
+        print("Break time")
+    breaktime() #goes to function pomodoro
+
+
+def breaktime():
+    t = 5
+    while t:
+        mins = t // 60
+        secs = t % 60
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Pomodoro Start")
+    appstart() #goes back to function appstart again
+
+
+appstart() #starts the function
